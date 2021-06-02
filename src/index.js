21b256355230dc2381ad57dedb935786b87468c8
Corinna S. (mariecori) <82397807+mariecori@users.noreply.github.com>
2021-06-02 17:03:38 +0200

    Adds forecast and Fahrenheit conversion, removes refresh button

--- src/index.js
+++ src/index.js
@@ -34,7 +34,15 @@ function formatDate(today) {
         minutes = `0${minutes}`;
     }
 
-    return `${day} ${date} ${month} ${year}, ${hours}:${minutes}`;
+    return `${day}, ${date} ${month} ${year}, ${hours}:${minutes}`;
+}
+
+//Get Forecast
+
+function getForecast(coordinates) {
+    let apiKey = "dd457ecaa9a81e873abe2de88465eeac";
+    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiURL).then(displayForecast);
 }
 
 //Search Engine Location
@@ -67,6 +75,8 @@ function inputWeather(response) {
         `icon/${response.data.weather[0].icon}.png`
     );
     iconElement.setAttribute("alt", response.data.weather[0].description);
+
+    getForecast(response.data.coord);
 }
 
 function citySearch(city) {
@@ -89,6 +99,22 @@ function displayFahrenheitTemp(event) {
     fahrenheitLink.classList.add("active");
     let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
     tempElement.innerHTML = Math.round(fahrenheitTemp);
+
+    if (isFahrenheit === false) {
+        let forecastTemp = document.querySelectorAll(".forecast-temp");
+        forecastTemp.forEach(function (fahrenheitTempElement) {
+            fahrenheitTempElement.innerHTML = Math.round(
+                (fahrenheitTempElement.innerHTML * 9) / 5 + 32
+            );
+        });
+    }
+
+    let degreeUnit = document.querySelectorAll(".degree");
+    degreeUnit.forEach(function (unitElement) {
+        unitElement.innerHTML = `°F`;
+    });
+
+    isFahrenheit = true;
 }
 
 //Celsius Conversion
@@ -98,6 +124,22 @@ function displayCelsiusTemp(event) {
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
     tempElement.innerHTML = Math.round(celsiusTemp);
+
+    if (isFahrenheit === true) {
+        let forecastTemp = document.querySelectorAll(".forecast-temp");
+        forecastTemp.forEach(function (celsiusTempElement) {
+            celsiusTempElement.innerHTML = Math.round(
+                ((celsiusTempElement.innerHTML - 32) * 5) / 9
+            );
+        });
+    }
+
+    let degreeUnit = document.querySelectorAll(".degree");
+    degreeUnit.forEach(function (unitElement) {
+        unitElement.innerHTML = `°C`;
+    });
+
+    isFahrenheit = false;
 }
 
 //Current Location
@@ -108,7 +150,6 @@ function showPosition(position) {
     let apiKey = "dd457ecaa9a81e873abe2de88465eeac";
 
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-    console.log(apiURL);
     axios.get(apiURL).then(inputWeather);
 }
 
@@ -116,8 +157,78 @@ function geolocation() {
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+//Format Forecast Day
+
+function formatForecastDay(timestamp) {
+    let forecastDate = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = days[forecastDate.getDay()];
+
+    let date = forecastDate.getDate();
+
+    let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    let month = months[forecastDate.getMonth()];
+
+    return `${day}<br />${date} ${month}`;
+}
+
+//Display Forecast
+
+function displayForecast(response) {
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6 && index > 0) {
+            forecastHTML =
+                forecastHTML +
+                `
+    <div class="col">
+                        <p id="forecast-day">${formatForecastDay(
+                            forecastDay.dt
+                        )}</p>
+                        <img
+                            src="icon/${forecastDay.weather[0].icon}.png"
+                            alt="current weather"
+                        />
+                        <br />
+                        <strong><span class="forecast-temp">${Math.round(
+                            forecastDay.temp.max
+                        )}</span><sup class="degree">°C</sup></strong>
+                        <br />
+                        <strong class="forecast-temp-min"><span class="forecast-temp">${Math.round(
+                            forecastDay.temp.min
+                        )}</span><sup class="degree">°C</sup></strong
+                        >
+                    </div>
+    `;
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+
+    forecastElement.innerHTML = forecastHTML;
+}
+
 //Global variables
 let celsiusTemp = null;
+
+let isFahrenheit = false;
 
 let now = new Date();
 let liDate = document.querySelector("#date");
